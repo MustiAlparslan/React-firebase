ffdcd2d95fc0cde4a7cc6c4a85215a624b96b631
Mustafa Alparslan <mustibatualp@gmail.com>
2022-09-03 12:59:28 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPwXi7SM6jAvemClmMPkjDcgRTZswUieE",
-  authDomain: "auth-30298.firebaseapp.com",
-  projectId: "auth-30298",
-  storageBucket: "auth-30298.appspot.com",
-  messagingSenderId: "230430713224",
-  appId: "1:230430713224:web:c6f24c75d6616faaff863d"
+  apiKey: ,
+  authDomain: //Your authDomain,
+  projectId: //Your project Id,
+  storageBucket: //Your StorageBucket,
+  messagingSenderId: //Your messagingSenderId,
+  appId: //your appId
 };
 
 const app = initializeApp(firebaseConfig);
